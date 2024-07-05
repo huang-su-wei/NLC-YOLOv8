3a522b6c6944fa6d42567f7e0dd25d8436b1c939
huang-su-wei <st9207130@gmail.com>
2024-07-05 17:01:46 +0800

    Delete v5-v6EfficientRep.py

--- v5-v6EfficientRep.py
+++
@@ -1,8 +0,0 @@
-_base_ = 'yolov5_s-v61_syncbn_8xb16-300e_coco.py'
-
-model = dict(
-    backbone=dict(
-        type='YOLOv6EfficientRep',
-        norm_cfg=dict(type='BN', momentum=0.03, eps=0.001),
-        act_cfg=dict(type='ReLU', inplace=True))
-)
